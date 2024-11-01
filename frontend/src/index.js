6de3c604d4ff118af0b8c7f275909c53eb7f89e0
Guerrerotech <guerrerotech.ia@gmail.com>
2024-11-01 18:34:54 -0500

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -10,7 +8,6 @@ root.render(
   <React.StrictMode>
    <BrowserRouter>
     <App />
-   </BrowserRouter>
   </React.StrictMode>
 );
 
